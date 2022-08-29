91b40816dc997704e89b51ff5eb174e2dc981e72
St Lk <stanislav.lukin@irlix.ru>
2022-08-29 18:40:39 +0400

    Try fixing problem with GH Pages. Try number 33

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from '@src/store';
 import App from '@src/App';
@@ -8,8 +8,8 @@ import App from '@src/App';
 const ROOT = ReactDOM.createRoot(document.getElementById('root'));
 ROOT.render(
     <Provider store={store}>
-        <BrowserRouter basename="/irlix_frontend_intensive">
+        <HashRouter basename="/irlix_frontend_intensive">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
